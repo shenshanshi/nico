5f8f42b7b1c5381e85e7171433bdcf6db6451ab2
绪运 莫 <2265364722@qq.com>
2023-04-01 15:48:30 +0800

    新建代码生成模块

---
+++ nico-generator/src/main/java/com/nico/controller/GeneratorController.java
@@ -0,0 +1,11 @@
+package com.nico.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tool/gen")
+public class GeneratorController {
+
+
+}
