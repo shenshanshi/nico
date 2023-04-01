83b73e08a52f6810214f624fc69b0e9fab88e595
绪运 莫 <2265364722@qq.com>
2023-04-01 15:48:04 +0800

    初始化项目

---
+++ nico-admin/src/main/java/com/nico/NicoApplication.java
@@ -0,0 +1,14 @@
+package com.nico;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicoApplication {
+     public static void main(String[] args) {
+         SpringApplication.run(NicoApplication.class, args);
+          System.out.println("\n------------nico项目启动成功------------\n");
+
+      }
+}
